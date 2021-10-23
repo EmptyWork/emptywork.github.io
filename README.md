27760c875a056ec06ec9c98057d53bef85a57eda
emptywork <22065214+EmptyWork@users.noreply.github.com>
2021-10-24 02:06:42 +0900

    update: re-styling work and project section

--- README.md
+++ README.md
@@ -10,5 +10,5 @@
 Source code for my personal website.<br/>
 The purpose of this repository is to showcase the code that I wrote<br/> and as a proof of my ability to work with Javascript, HTML5 and CSS3.
 
-  </div>
+</div>
 
--- js/main.js
+++ js/main.js
@@ -1,89 +1,55 @@
-// mobileShow
-let mobileButton = document.querySelector("[data-mobile-menu-button]")
-mobileButton.addEventListener("click", () => {
-  let mobileMenuSection = document.querySelector("[data-mobile-menu-section]")
-  if (mobileMenuSection.classList.contains("not-showing")) {
-    mobileMenuSection.classList.remove("not-showing")
-    setTimeout(() => document.body.classList.add("no-scroll"), 300)
-    return
-  }
-  mobileMenuSection.classList.add("not-showing")
-  document.body.classList.remove("no-scroll")
-  return
-})
-
+/**
+ * Fetching data from the said url
+ * @param {string} url
+ */
 const dataLoader = async (url) => {
   const response = await fetch(url)
   const data = await response.json()
-  if(response) {
+  if (response) {
     schedulerLoader(data.schedules)
     projectsLoader(data.works)
   }
 }
 
-dataLoader('../data.json')
+dataLoader("../data.json")
 
-// schedulerLoader
-let currentDate = new Date()
-const schedulerLoader = (schedules) => {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ]
-  let replaceStatus = document.querySelector("#activity")
-  let currentDay = currentDate.getDay()
+/**
+ * Mobile hamburger menu logic
+ */
+let mobileButton = document.querySelector("[data-mobile-menu-button]")
+let mobileMenuSection = document.querySelector("[data-mobile-menu-section]")
 
-  for(let i = 0; i < schedules.length; i++) {
-    if(days[currentDay] === schedules[i]) {
-      replaceStatus.textContent = "Kuliah";
-      break
-    }
-    replaceStatus.textContent = "Bebas"
+mobileButton.addEventListener("click", () => {
+  if (mobileMenuSection.classList.contains("not-showing")) {
+    setShowing()
+    return
   }
+  setHidden()
+  return
+})
+
+const setShowing = () => {
+  mobileMenuSection.classList.remove("not-showing")
+  setTimeout(() => document.body.classList.add("no-scroll"), 300)
 }
 
-const projectsLoader = (projects) => {
-  let worksSection = document.querySelector('.projects')
-  projects.forEach(project => {
-    createNewProject(project, worksSection)
-  })
+const setHidden = () => {
+  mobileMenuSection.classList.add("not-showing")
+  document.body.classList.remove("no-scroll")
 }
 
-const createNewProject = ({name, started_at, finished_at, details, links, image}, workSection) => {
-  let projectLink = document.createElement('a');
-  projectLink.href = "#projects";
-  projectLink.setAttribute('arial-label', `Project: ${name}`)
-  let projectContainer = document.createElement('figure')
-  projectContainer.classList.add('project')
-  if(image)projectContainer.style.backgroundImage = `linear-gradient(transparent, var(--clr-accent-dark)), url(${image})`;
-  projectContainer.innerHTML = `
-  <figcaption>
-    <header>
-      <h2>${name}</h2> 
-      <small>${(finished_at != null) ? `${finished_at} `: `${started_at} — Present` }</small>
-    </header>
-    <article class="description-area">
-      <p>${details}</p>
-    </article>
-    <ul class="more-links">
-    ${(links) ? `
-      <li><a href="${links[0]}" target="_blank" class="sourcecode">sourcecode</a></li>
-      <li><a href="${links[1]}" target="_blank" class="sourcecode">live-demo</a></li>
-      ` : `
-      <li><a href="#projects" disabled class="sourcecode">sourcecode</a></li>
-      <li><a href="#projects" disabled class="sourcecode">live-demo</a></li>
-      `}
-    </ul>
-    </figcaption>
-    `
-  projectLink.appendChild(projectContainer)
-  workSection.appendChild(projectLink)
-}
+/**
+ * Global Variables for Date related functions
+ */
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+]
 
 let months = [
   "January",
@@ -99,11 +65,119 @@ let months = [
   "November",
   "December",
 ]
+
+let replaceStatus = document.querySelector("#activity")
 let replaceMonth = document.querySelector("#month")
+
+let currentDate = new Date()
+let currentDay = currentDate.getDay()
 let currentMonth = currentDate.getMonth()
+
 replaceMonth.textContent = months[currentMonth]
 
-// End Line
+/**
+ * Updating activity status based on array of schedule
+ * @param {array} schedules
+ */
+
+const schedulerLoader = (schedules) => {
+  for (let i = 0; i < schedules.length; i++) {
+    if (days[currentDay] === schedules[i]) {
+      replaceStatus.textContent = "Kuliah"
+      break
+    }
+    replaceStatus.textContent = "Bebas"
+  }
+}
+
+/**
+ * Loading the list of project div based on the array of project
+ * @param {array} projects
+ */
+
+const projectsLoader = (projects) => {
+  let worksSection = document.querySelector(".projects")
+  projects.forEach((project) => {
+    createNewProject(project, worksSection)
+  })
+}
+
+/**
+ * Creating a new figure from the project object
+ * @param {object} project
+ * @param {any} workSection
+ */
+
+const createNewProject = (
+  { name, started_at, finished_at, details, links, image },
+  workSection
+) => {
+  let projectLink = document.createElement("a")
+  projectLink.href = "#projects"
+  projectLink.classList.add('projectlink')
+  projectLink.setAttribute("arial-label", `Project: ${name}`)
+  let projectContainer = document.createElement("figure")
+  projectContainer.classList.add("project", "t-all")
+  if (image)
+    projectContainer.style.backgroundImage = `linear-gradient(transparent, var(--clr-accent-dark)), url(${image})`
+  projectContainer.innerHTML = `
+  <figcaption>
+    <header>
+      <h2>${name}</h2> 
+      <small>${
+        finished_at != null ? `${finished_at} ` : `${started_at} — Present`
+      }</small>
+    </header>
+    <article class="description-area">
+      <p>${details}</p>
+    </article>
+    <ul class="more-links">
+    ${
+      links
+        ? `
+      <li><a href="${links[0]}" target="_blank" class="sourcecode">sourcecode</a></li>
+      <li><a href="${links[1]}" target="_blank" class="sourcecode">demo</a></li>
+      `
+        : `
+      <li><a href="#projects" disabled class="sourcecode">sourcecode</a></li>
+      <li><a href="#projects" disabled class="sourcecode">demo</a></li>
+      `
+    }
+    </ul>
+    </figcaption>
+    `
+  projectLink.appendChild(projectContainer)
+  workSection.appendChild(projectLink)
+}
+
+/**
+ * Changing the theme based on user toggle
+ */
+let themesButton = document.querySelector("[data-theme-button]")
+
+const themeHandler = () => {
+  if (document.documentElement.classList.contains("dark")) {
+    setLight()
+    return
+  }
+  setDark()
+  return
+}
+themesButton.addEventListener("click", themeHandler)
+
+const setLight = () => {
+  document.documentElement.classList.remove("dark")
+  localStorage.theme = "light"
+}
+
+const setDark = () => {
+  document.documentElement.classList.add("dark")
+  localStorage.theme = "dark"
+}
+
+/**
+ * Printing details on console
+ */
 console.group("Emptywork -- Dev")
 console.log(
   "%cEmptywork %c console",
