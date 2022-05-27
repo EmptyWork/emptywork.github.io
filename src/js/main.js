762d954075bb356690f8b414b44227739196b8ed
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-05-28 00:46:57 +0900

    update: refactor `console.logs`

--- src/js/main.js
+++ src/js/main.js
@@ -24,7 +24,7 @@ let mobileButtons = document.querySelectorAll("[data-mobile-menu-button]")
 let mobileMenuSection = document.querySelector("[data-mobile-menu-section]")
 let mobileMenuLinks = document.querySelectorAll("body>ul>li")
 
-mobileButtons.forEach( button => {
+mobileButtons.forEach((button) => {
   button.addEventListener("click", () => {
     if (mobileMenuSection.classList.contains("not-showing")) {
       setShowing()
@@ -87,81 +87,86 @@ const setDark = () => {
 /**
  * Printing details on console
  */
-console.group("Emptywork -- Dev")
+
 console.log(
-  "%cEmptywork %c console",
-  "background-color:#cecece; margin:2px; color:transparent; font-size:0.7rem"
+  "%cEmptywork Console%c\nemptywork dot github dot io%c\n emptywork dot github dot io%c\nEnd of Console%c\n NOTE %c DON'T COPY ANY CODE INTO HERE",
+  "background-color:#cecece; margin: 0.15em; padding: 0.1em; border-radius: 0.2em; color:transparent; font-size:1rem",
+  "background-color:rebeccapurple; margin: 0.15em; padding: 0.1em; border-radius: 0.2em; color:transparent; font-size:1rem",
+  "font-size:1.2rem",
+  "background-color:#cecece; margin: 0.15em; padding: 0.1em; border-radius: 0.2em; color:transparent; font-size:1rem",
+  "background-color:rebeccapurple; margin: 0.15em; padding: 0.1em; border-radius: 0.2em; font-size:1rem",
+  "color: rebeccapurple; font-size: 1.1rem"
 )
-console.log(
-  "%cdot one %c com",
-  "background-color:rebeccapurple; margin:2px; color:transparent; font-size:0.7rem"
-)
-console.log("%cemptywork dot github dot io", "font-size:0.88rem")
-console.log(
-  "%cEmptywork Console %c testing",
-  "background-color:#cecece; margin:2px; color:transparent; font-size:0.7rem"
-)
-console.groupEnd()
 
 /**
  * Global Variables for Date related functions
  */
- let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+]
 
- let months = [
-   "January",
-   "Febuary",
-   "March",
-   "April",
-   "May",
-   "June",
-   "July",
-   "August",
-   "September",
-   "October",
-   "November",
-   "December",
- ]
- 
- let replaceStatus = document.querySelector("#activity")
- let replaceMonth = document.querySelector("#month")
- let replaceYear = document.querySelector('#year')
- 
- let currentDate = new Date()
- let currentDay = currentDate.getDay()
- let currentMonth = currentDate.getMonth()
- let currentYear = currentDate.getFullYear()
- 
- if(replaceMonth) replaceMonth.textContent = months[currentMonth]
- if(replaceYear) replaceYear.textContent = currentYear
- 
- /**
-  * Updating activity status based on array of schedule
-  * @param {array} schedules
-  */
- 
- const schedulerLoader = (schedules = {}) => {
-   let time = new Date()
-   let currentTime = time.getUTCHours() + 9
-   if(currentTime > 23) currentTime -= 23
-   
-   replaceStatus.textContent = "Idle"
+let months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+]
 
-   setTimeout(() => {
-     for (let i = 0; i < schedules.length; i++) {
-       let {day, endHour, startHour} = schedules[i]
-       if (!replaceStatus) return
-       
-       if (days[currentDay] === toCapitalizeWord(day)) {
-         if(currentTime > startHour  && currentTime < endHour) {replaceStatus.textContent = "Studying"}
-         break
-       }
-       replaceStatus.textContent = "Idle"
-     }
-   }, 0)
- }
+let replaceStatus = document.querySelector("#activity")
+let replaceMonth = document.querySelector("#month")
+let replaceYear = document.querySelector("#year")
+
+let currentDate = new Date()
+let currentDay = currentDate.getDay()
+let currentMonth = currentDate.getMonth()
+let currentYear = currentDate.getFullYear()
+
+if (replaceMonth) replaceMonth.textContent = months[currentMonth]
+if (replaceYear) replaceYear.textContent = currentYear
+
+/**
+ * Updating activity status based on array of schedule
+ * @param {array} schedules
+ */
+
+const schedulerLoader = (schedules = {}) => {
+  let time = new Date()
+  let currentTime = time.getUTCHours() + 9
+  if (currentTime > 23) currentTime -= 23
+
+  if (replaceStatus) replaceStatus.textContent = "Idle"
+
+  setTimeout(() => {
+    for (let i = 0; i < schedules.length; i++) {
+      let { day, endHour, startHour } = schedules[i]
+      if (!replaceStatus) return
+
+      if (days[currentDay] === toCapitalizeWord(day)) {
+        if (currentTime > startHour && currentTime < endHour) {
+          replaceStatus.textContent = "Studying"
+        }
+        break
+      }
+      replaceStatus.textContent = "Idle"
+    }
+  }, 0)
+}
 
 // Injecting #main to main that doesn't have #main
-let mainElem = document.querySelector('#main')
-if(!mainElem) mainElem = document.querySelector('.main')
-mainElem.id = 'main'
+let mainElem = document.querySelector("#main")
+if (!mainElem) mainElem = document.querySelector(".main")
+mainElem.id = "main"
