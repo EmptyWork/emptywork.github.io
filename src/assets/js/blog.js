0fd046c0a7d5ab97f95fba4d93feb9ebb7048f0a
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2025-06-19 08:55:57 +0900

    feat: `skipTo` — after the table of contents

--- src/assets/js/blog.js
+++ src/assets/js/blog.js
@@ -1,2 +1,24 @@
+const checkTableOfContents = () => {
+    const toc = document.getElementById("main").querySelector(".table-of-contents")
+    const skipToMain = document.querySelector('a[href="#main"]')
+    const navSkip = document.querySelector(".nav-skip")
+    if (!toc) return
+
+    let nextCandidate = toc?.nextElementSibling
+    nextCandidate.id = "pass-toc"
+
+    const skipLink = createSkipLink(nextCandidate.id)
+    navSkip.insertBefore(skipLink, skipToMain.nextSibling)
+}
+
+const createSkipLink = (id, text = "Skip pass the Table of Contents") => {
+    const skipLink = document.createElement('a')
+    skipLink.className = 'nav-skipto'
+    skipLink.href = `#${id}`
+    skipLink.innerText = text
+    return skipLink
+}
+
+checkTableOfContents()
 Array.from(document.links).filter(link => link.hostname != window.location.hostname)
     .forEach(link => link.target = '_blank')
