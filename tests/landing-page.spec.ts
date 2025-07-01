aa64a70756984706295ea2aa071a695628b0972b
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2025-07-01 10:17:01 +0900

    update: `landing-page.spec.ts` — only handle on download test

--- tests/landing-page.spec.ts
+++ tests/landing-page.spec.ts
@@ -5,26 +5,14 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/EmptyWork/);
 });
 
-test('find a curriculum vitae and download', async ({ page, browserName }, testInfo) => {
-  const isHeaded = testInfo.project.name.includes('headed')
+test('find a curriculum vitae and download', async ({ page }) => {
 
   await page.goto('/');
 
-  if (browserName === 'chromium' && isHeaded) {
-    const [popup] = await Promise.all([
-      page.waitForEvent('popup'),
-      await page.getByRole('link', { name: 'Open PDF of my Curriculum' }).click(),
-    ]);
-    await popup.waitForLoadState('domcontentloaded');
-    const url = popup.url();
-    return expect(url).toMatch(/\.pdf$/);
-  }
-
-  const [download] = await Promise.all([
-    page.waitForEvent('download'),
-    page.getByRole('link', { name: 'Open PDF of my Curriculum' }).click(),
-  ]);
-
+  const downloadPromise = page.waitForEvent('download');
+  await page.getByRole('link', { name: 'Download my Curriculum Vitae' }).click();
+  const download = await downloadPromise;
+  
   const fileName = download.suggestedFilename();
   expect(fileName).toMatch(/\.pdf$/);
 
