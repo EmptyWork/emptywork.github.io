d2f888781b7094ec9a0816ae981c9375db1c6cc7
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2025-07-02 09:55:38 +0900

    update: `playwright.config.ts` — use `npm start`

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'npm run dev',
+    command: 'npm start',
     url: localhost,
     reuseExistingServer: !process.env.CI,
   },
